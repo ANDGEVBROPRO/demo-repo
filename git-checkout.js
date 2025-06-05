59578f452ba29c362014d05c871f4335f09eb2d5
ANDGEVBROPRO <ando.gevoryan.07@gmail.com>
2025-06-05 22:06:30 +0400

    version 1 updated
    the lastest

--- git-checkout.js
+++ git-checkout.js
@@ -1,6 +1,4 @@
-console.log("'git checkout --'to remove all the change we made in the file")
+console.log("git checkout -- to remove all the change we made in the file")
 
 
 console.log("git reset to remove form the Staged")
-
-console.log("we can switch between commits we created by using checkout ")
