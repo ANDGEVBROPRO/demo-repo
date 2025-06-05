30531237b4fee965f65d635fc856de77bda6622c
ANDGEVBROPRO <ando.gevoryan.07@gmail.com>
2025-06-05 20:35:59 +0400

    new version 2
    this is second version

--- change.js
+++ change.js
@@ -1,4 +1,4 @@
 console.log("--amned")
 
-console.log("using --amned, allowing use to not creating a new commit ")
+console.log("using --amend, allowing use to not creating a new commit ")
 console.log('but change the previus commited files, if we made a mistake')
