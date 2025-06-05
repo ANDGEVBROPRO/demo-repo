d19d51226988f37b91cb66bb27c2e3d12c9aa638
ANDGEVBROPRO <ando.gevoryan.07@gmail.com>
2025-06-05 20:06:35 +0400

    new version

---
+++ change.js
@@ -0,0 +1,4 @@
+console.log("--amned")
+
+console.log("using --amned, allowing use to not creating a new commit ")
+console.log('but change the previus commited files, if we made a mistake')
---
+++ log-histor.js
@@ -0,0 +1,4 @@
+console.log("git log")
+
+console.log("allow us to check our history of works")
+console.log("to exit the log, and continue to work just press 'q' ")
